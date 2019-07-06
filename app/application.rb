4485d4a876a1c0079a6ea30a67365a0f0358ea34
elijahmm17 <elijahmm17@gmail.com>
2019-07-06 19:16:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,7 @@ class Application
     utc = Time.new
     offset = 25200
     time = utc - offset
+    t_num = time.to_s.split(" ")[1].split(":").join("").to_i
     if time > 120000 
       resp.write "Good Afternoon!" 
     else 
