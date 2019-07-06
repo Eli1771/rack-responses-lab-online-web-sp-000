34ac4d6fbefb09cb5d5199fc32d9900ab962c614
elijahmm17 <elijahmm17@gmail.com>
2019-07-06 19:19:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,8 @@
 class Application 
   def call(env) 
     resp = Rack::Response.new 
-    utc = Time.new
-    offset = 25200
-    time = utc - offset
-    t_num = time.to_s.split(" ")[1].split(":").join("").to_i
-    if t_num > 120000 
+    time = Time.now
+    if time.hour > 12 
       resp.write "Good Afternoon!" 
     else 
       resp.write "Good Morning!" 
